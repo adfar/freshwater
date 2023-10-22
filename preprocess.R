35f9104e629fa11b85f2abefe4d13efc464b96cc
adfar <adf5092@gmail.com>
2023-10-22 14:25:55 -0400

    comment attempt

--- preprocess.R
+++ preprocess.R
@@ -7,10 +7,21 @@ lakes <- read_html("https://en.wikipedia.org/wiki/List_of_lakes_by_volume") |>
 lakes <- lakes[[2]] |> 
   html_table() |> 
   clean_names() |> 
-  filter(salinity == "Fresh")
+  filter(salinity == "Fresh") |> 
+  #remove superscript in water_volume for Vostok
+  mutate(water_volume = ifelse(name == "Vostok", "5,400±1,600 km3 (~1,300 cu mi)", water_volume))
 
+
+
+#remove references
 lakes$name <- sub("\\[.*\\]", "", lakes$name)
+
+#separate us and metric
 lakes <- lakes |> 
-  separate(col = surface_area, into = c("km2", "mi2"), sep = " km2 ")
+  separate(col = surface_area, into = c("km2", "mi2"), sep = "km2") |> 
+  separate(col = water_volume, into = c("km3", "mi3"), sep = "km3") |> #vostok has a superscript so km3 doesn't work
+  mutate(km2 = str_trim(km2), mi2 = gsub("[^0-9~,]+", "", mi2), km3 = str_trim(km3), mi3 = gsub("[^0-9~,]+", "", mi3))
+
+
 
 
