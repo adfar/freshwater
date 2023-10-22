10502fe1130e64ef24a0a7774376bbf029851441
adfar <adf5092@gmail.com>
2023-10-22 19:00:23 -0400

    add table and unit control

--- app.R
+++ app.R
@@ -1,11 +1,44 @@
 library(shiny)
 library(tidyverse)
+library(DT)
+
+lakes <- read_csv("data/lakes.csv")
 
 ui <- fluidPage(
-  
+  titlePanel("Lakes!"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("units", "Units", choices = c("Kilometers", "Miles"))
+    ),
+    mainPanel(
+      DTOutput("water")
+    )
+  )
 )
 
 server <- function(input, output, session) {
+  selected <- reactive(
+      if (input$units == "Kilometers") {
+        lakes |> 
+          select(name, country, region, km2, km3) |> 
+          rename(Name = name, Country = country, Region = region, Area = km2, Volume = km3)
+      } else if (input$units == "Miles") {
+        lakes |> 
+          select(name, country, region, mi2, mi3) |> 
+          rename(Name = name, Country = country, Region = region, Area = mi2, Volume = mi3)
+    }
+  )
+
+  
+  output$water <- renderDT({
+    selected()
+  })
+  
+  output$plot <- renderPlot(
+    selected() |> 
+      ggplot(aes(Area, Volume)) +
+      geom_point()
+  )
   
 }
 
